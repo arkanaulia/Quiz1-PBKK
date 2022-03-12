3a12caf3cda823b09743264d0685808f3c2b6e99
unknown <arkanaulia@gmail.com>
2022-03-12 18:27:32 +0700

    Fresh Commit

---
+++ Pages/q1/collaboration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace collaboration.Pages
+{
+    public class collaborationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/q1/service.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace service.Pages
+{
+    public class serviceModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
